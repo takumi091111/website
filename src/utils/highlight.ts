d89989d786e2d9f689f66cc580d6d82725a4ed62
Asamac <takumi091111@gmail.com>
2019-06-26 21:01:18 +0900

    fix

--- src/utils/highlight.ts
+++ src/utils/highlight.ts
@@ -1,50 +1,23 @@
 const hljs = require('highlight.js/lib/highlight')
 
-/* eslint @typescript-eslint/no-var-require: 0 */
-hljs.registerLanguage(
-  'cs',
-  require('highlight.js/lib/languages/cs')
-)
-hljs.registerLanguage(
-  'css',
-  require('highlight.js/lib/languages/css')
-)
-hljs.registerLanguage(
-  'go',
-  require('highlight.js/lib/languages/go')
-)
-hljs.registerLanguage(
-  'java',
-  require('highlight.js/lib/languages/java')
-)
+/* eslint @typescript-eslint/no-var-requires: 0 */
+hljs.registerLanguage('cs', require('highlight.js/lib/languages/cs'))
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+hljs.registerLanguage('go', require('highlight.js/lib/languages/go'))
+hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
 hljs.registerLanguage(
   'javascript',
   require('highlight.js/lib/languages/javascript')
 )
-hljs.registerLanguage(
-  'json',
-  require('highlight.js/lib/languages/json')
-)
-hljs.registerLanguage(
-  'php',
-  require('highlight.js/lib/languages/php')
-)
-hljs.registerLanguage(
-  'ruby',
-  require('highlight.js/lib/languages/ruby')
-)
-hljs.registerLanguage(
-  'shell',
-  require('highlight.js/lib/languages/shell')
-)
+hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
+hljs.registerLanguage('php', require('highlight.js/lib/languages/php'))
+hljs.registerLanguage('ruby', require('highlight.js/lib/languages/ruby'))
+hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
 // hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
 hljs.registerLanguage(
   'typescript',
   require('highlight.js/lib/languages/typescript')
 )
-hljs.registerLanguage(
-  'xml',
-  require('highlight.js/lib/languages/xml')
-)
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
 
 export { hljs }
