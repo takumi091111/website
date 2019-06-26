14e2823464a4d8b82096d918b3ff07cee1559e9b
Asamac <takumi091111@gmail.com>
2019-06-26 20:24:44 +0900

    requireからimportへ変更 型定義の作成

--- src/utils/highlight.ts
+++ src/utils/highlight.ts
@@ -1,22 +1,50 @@
-const hljs = require('highlight.js/lib/highlight')
+import hljs from 'highlight.js/lib/highlight'
 
-hljs.registerLanguage('cs', require('highlight.js/lib/languages/cs'))
-hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
-hljs.registerLanguage('go', require('highlight.js/lib/languages/go'))
-hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
+/* eslint @typescript-eslint/explicit-function-return-type: 0 */
+hljs.registerLanguage(
+  'cs',
+  import('highlight.js/lib/languages/cs').then(m => m.default)
+)
+hljs.registerLanguage(
+  'css',
+  import('highlight.js/lib/languages/css').then(m => m.default)
+)
+hljs.registerLanguage(
+  'go',
+  import('highlight.js/lib/languages/go').then(m => m.default)
+)
+hljs.registerLanguage(
+  'java',
+  import('highlight.js/lib/languages/java').then(m => m.default)
+)
 hljs.registerLanguage(
   'javascript',
-  require('highlight.js/lib/languages/javascript')
+  import('highlight.js/lib/languages/javascript').then(m => m.default)
 )
-hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
-hljs.registerLanguage('php', require('highlight.js/lib/languages/php'))
-hljs.registerLanguage('ruby', require('highlight.js/lib/languages/ruby'))
-hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
-// hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
+hljs.registerLanguage(
+  'json',
+  import('highlight.js/lib/languages/json').then(m => m.default)
+)
+hljs.registerLanguage(
+  'php',
+  import('highlight.js/lib/languages/php').then(m => m.default)
+)
+hljs.registerLanguage(
+  'ruby',
+  import('highlight.js/lib/languages/ruby').then(m => m.default)
+)
+hljs.registerLanguage(
+  'shell',
+  import('highlight.js/lib/languages/shell').then(m => m.default)
+)
+// hljs.registerLanguage('sql', import('highlight.js/lib/languages/sql'))
 hljs.registerLanguage(
   'typescript',
-  require('highlight.js/lib/languages/typescript')
+  import('highlight.js/lib/languages/typescript').then(m => m.default)
 )
-hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+hljs.registerLanguage(
+  'xml',
+  import('highlight.js/lib/languages/xml').then(m => m.default)
+)
 
 export { hljs }
